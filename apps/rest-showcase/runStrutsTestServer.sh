4bfa3d4f84b179c4d3633086a99e506c8e938f94
Jonathan Bell <jon@jonbell.net>
2020-01-26 16:01:14 -0500

    Switch server start script to use 1.0.0 release

--- apps/rest-showcase/runStrutsTestServer.sh
+++ apps/rest-showcase/runStrutsTestServer.sh
@@ -8,11 +8,12 @@ AUTO_TAINT="-Dphosphor.sources=${SOURCES} -Dphosphor.sinks=${SINKS} -Dphosphor.t
 INST_JAVA="${HOME}/.phosphor-jvm/bin/java"
 M2_REPO="${HOME}/.m2/repository"
 PHOSPHOR_JAR="${M2_REPO}/edu/gmu/swe/phosphor/Phosphor/0.0.4-SNAPSHOT/Phosphor-0.0.4-SNAPSHOT.jar"
-RIVULET_CORE_JAR="${M2_REPO}/io/rivulet/rivulet-core/1.0.0-SNAPSHOT/rivulet-core-1.0.0-SNAPSHOT.jar"
-SERVER_JAR="${M2_REPO}/io/rivulet/embedded-server/1.0.0-SNAPSHOT/embedded-server-1.0.0-SNAPSHOT.jar"
+RIVULET_CORE_JAR="${M2_REPO}/io/rivulet/rivulet-core/1.0.0/rivulet-core-1.0.0.jar"
+SERVER_JAR="${M2_REPO}/io/rivulet/embedded-server/1.0.0/embedded-server-1.0.0.jar"
 PHOSPHOR_OPTS="taintSourceWrapper=io.rivulet.internal.RivuletAutoTaintWrapper,ignore=io/rivulet/internal/,arrayindex,priorClassVisitor=io.rivulet.internal.RivuletCV,ignoredMethod=org/mindrot/jbcrypt/BCrypt.encipher([II)V,taintSources=${SOURCES},taintSinks=${SINKS},taintThrough=${TAINT_THROUGHS}"
-JAVA_FLAGS="-Xmx8g -DphosphorCacheDirectory=cached-phosphor -Xbootclasspath/p:${PHOSPHOR_JAR}:${RIVULET_CORE_JAR} -javaagent:${PHOSPHOR_JAR}=${PHOSPHOR_OPTS}"
+JAVA_FLAGS="-Xmx8g -DphosphorCacheDirectory=cached-phosphor -Xbootclasspath/p:${PHOSPHOR_JAR}:${RIVULET_CORE_JAR} -javaagent:${PHOSPHOR_JAR}=${PHOSPHOR_OPTS}" 
 
+#JAVA_FLAGS="${JAVA_FLAGS} -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 
 USER_FLAGS="-Difc.criticalReproductionFiles"
 CMD="$INST_JAVA $JAVA_FLAGS -cp $SERVER_JAR $USER_FLAGS io.rivulet.internal.server.EmbeddedServer 8080 8182 target/struts2-rest-showcase.war struts2-rest-showcase"
